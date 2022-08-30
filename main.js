033f609f7adb2c2e81044df22b50f82417d86a2f
Patrick K <48802394+Z3r0cks@users.noreply.github.com>
2022-08-30 17:57:00 +0200

    TASK: structre

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-console.log('Hello World');
+const accordionList = document.getElementsByClassName("accordion");
+for (let i = 0; i < accordionList.length; i++) {
+    accordionList[i].addEventListener("click", function () {
+        const arrow = this.firstElementChild;
+        const parent = this.parentElement.parentElement;
+        this.classList.toggle("active");
+        const panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            toggleSingleArr(arrow);
+            panel.style.maxHeight = null;
+        }
+        else {
+            toggleSingleArr(arrow);
+            panel.style.maxHeight = panel.scrollHeight + "px";
+            parent.style.maxHeight = parent.scrollHeight + panel.scrollHeight + "px";
+        }
+    });
+}
+function toggleSingleArr(arrow) {
+    if (arrow.className.includes('right', 1))
+        arrow.className = "arrow down";
+    else
+        arrow.className = "arrow right";
+}
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,1 +1,25 @@
-console.log('Hello World');
+const accordionList = document.getElementsByClassName("accordion");
+
+for (let i: number = 0; i < accordionList.length; i++) {
+   accordionList[i].addEventListener("click", function () {
+      const arrow: HTMLSpanElement = this.firstElementChild
+      const parent = this.parentElement.parentElement;
+      this.classList.toggle("active");
+      const panel: HTMLDivElement = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+         toggleSingleArr(arrow);
+         panel.style.maxHeight = null;
+      } else {
+         toggleSingleArr(arrow);
+         panel.style.maxHeight = panel.scrollHeight + "px";
+         parent.style.maxHeight = parent.scrollHeight + panel.scrollHeight + "px";
+      }
+   });
+}
+
+function toggleSingleArr(arrow: HTMLSpanElement) {
+   if (arrow.className.includes('right', 1))
+      arrow.className = "arrow down";
+   else
+      arrow.className = "arrow right";
+}
