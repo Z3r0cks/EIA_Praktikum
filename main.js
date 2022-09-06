f5d2c0e4609edd60300c1b76b991fbc2cbf64caf
Patrick K <48802394+Z3r0cks@users.noreply.github.com>
2022-09-06 22:09:17 +0200

    WIP: some style & content

--- main.js
+++ main.js
@@ -1,20 +1,68 @@
 const accordionList = document.getElementsByClassName("accordion");
+const navListIds = ["introduction", "vsc", "github", "git", "practical", "evaluation", "subission"];
+const goTopBtn = document.getElementById("goTopBtn");
+const collapseBtn = document.getElementById("collapseBtn");
+const headerBtn = document.getElementById("title");
+const sideNavBtn = document.getElementById("sideNavBtn");
+const navList = document.querySelector(".navList");
+goTopBtn.addEventListener("click", () => {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+});
+collapseBtn.addEventListener("click", () => {
+    closeAll();
+});
+headerBtn.addEventListener("click", () => {
+    goTopBtn.click();
+    closeAll();
+});
+console.log(sideNavBtn);
+sideNavBtn.addEventListener("click", () => {
+    if (sideNavBtn.firstElementChild.className != "navListRight") {
+        console.log("isLeft");
+        navList.style.left = "0rem";
+        sideNavBtn.firstElementChild.classList.add("navListRight");
+    }
+    else {
+        console.log("isRight");
+        navList.style.left = "-17.6rem";
+        sideNavBtn.firstElementChild.classList.remove("navListRight");
+    }
+});
 for (let i = 0; i < accordionList.length; i++) {
     accordionList[i].addEventListener("click", function () {
-        const arrow = this.firstElementChild;
-        const parent = this.parentElement.parentElement;
-        this.classList.toggle("active");
-        const panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            toggleSingleArr(arrow);
-            panel.style.maxHeight = null;
-        }
-        else {
-            toggleSingleArr(arrow);
-            panel.style.maxHeight = panel.scrollHeight + "px";
-            parent.style.maxHeight = parent.scrollHeight + panel.scrollHeight + "px";
-        }
+        toggleArr(this.firstElementChild, this);
     });
+}
+navListIds.forEach(el => {
+    const content = document.getElementById(`${el}`);
+    const nav = document.getElementById(`${el}Nav`);
+    nav.addEventListener('click', () => {
+        closeAll();
+        toggleArr(content.firstElementChild, content);
+        setTimeout(() => {
+            const pos = document.getElementById(el).offsetTop - 100;
+            window.scrollTo({ top: pos, behavior: "smooth" });
+        }, 350);
+    });
+});
+function closeAll() {
+    for (let i = 0; i < accordionList.length; i++) {
+        accordionList[i].firstElementChild.className = "arrow right";
+        accordionList[i].nextElementSibling.style.maxHeight = null;
+    }
+}
+function toggleArr(arrow, content) {
+    const parent = content.parentElement.parentElement;
+    // content.classList.toggle("active");
+    const panel = content.nextElementSibling;
+    toggleSingleArr(arrow);
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+    }
+    else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        parent.style.maxHeight = parent.scrollHeight + panel.scrollHeight + "px";
+    }
 }
 function toggleSingleArr(arrow) {
     if (arrow.className.includes('right', 1))
--- main.ts
+++ main.ts
@@ -1,20 +1,74 @@
 const accordionList = document.getElementsByClassName("accordion");
+const navListIds = ["introduction", "vsc", "github", "git", "practical", "evaluation", "subission"];
+const goTopBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("goTopBtn");
+const collapseBtn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("collapseBtn");
+const headerBtn: HTMLAnchorElement = <HTMLAnchorElement>document.getElementById("title");
+const sideNavBtn: HTMLDivElement = <HTMLDivElement>document.getElementById("sideNavBtn");
+const navList: HTMLDivElement = <HTMLDivElement>document.querySelector(".navList");
+
+goTopBtn.addEventListener("click", () => {
+   window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+})
+
+collapseBtn.addEventListener("click", () => {
+   closeAll();
+})
+
+headerBtn.addEventListener("click", () => {
+   goTopBtn.click();
+   closeAll();
+})
+
+console.log(sideNavBtn);
+sideNavBtn.addEventListener("click", () => {
+   if (sideNavBtn.firstElementChild.className != "navListRight") {
+      console.log("isLeft");
+      navList.style.left = "0rem";
+      sideNavBtn.firstElementChild.classList.add("navListRight");
+   } else {
+      console.log("isRight");
+      navList.style.left = "-17.6rem";
+      sideNavBtn.firstElementChild.classList.remove("navListRight");
+   }
+})
 
 for (let i: number = 0; i < accordionList.length; i++) {
    accordionList[i].addEventListener("click", function () {
-      const arrow: HTMLSpanElement = this.firstElementChild
-      const parent = this.parentElement.parentElement;
-      this.classList.toggle("active");
-      const panel: HTMLDivElement = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-         toggleSingleArr(arrow);
-         panel.style.maxHeight = null;
-      } else {
-         toggleSingleArr(arrow);
-         panel.style.maxHeight = panel.scrollHeight + "px";
-         parent.style.maxHeight = parent.scrollHeight + panel.scrollHeight + "px";
-      }
+      toggleArr(this.firstElementChild, this);
    });
+}
+
+navListIds.forEach(el => {
+   const content: HTMLHeadElement = document.getElementById(`${el}`);
+   const nav: HTMLHeadElement = document.getElementById(`${el}Nav`);
+   nav.addEventListener('click', () => {
+      closeAll();
+      toggleArr(content.firstElementChild as HTMLSpanElement, content);
+      setTimeout(() => {
+         const pos: number = document.getElementById(el).offsetTop - 100;
+         window.scrollTo({ top: pos, behavior: "smooth" });
+      }, 350);
+   })
+})
+
+function closeAll() {
+   for (let i: number = 0; i < accordionList.length; i++) {
+      (accordionList[i].firstElementChild as HTMLSpanElement).className = "arrow right";
+      (accordionList[i].nextElementSibling as HTMLDivElement).style.maxHeight = null;
+   }
+}
+
+function toggleArr(arrow: HTMLSpanElement, content: HTMLHeadElement) {
+   const parent = content.parentElement.parentElement
+   // content.classList.toggle("active");
+   const panel: HTMLDivElement = <HTMLDivElement>content.nextElementSibling;
+   toggleSingleArr(arrow);
+   if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+   } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      parent.style.maxHeight = parent.scrollHeight + panel.scrollHeight + "px";
+   }
 }
 
 function toggleSingleArr(arrow: HTMLSpanElement) {
